1e60aaaeb7555e04052d453e191f6b80aaf57573
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-26 09:26:02 -0500

    add basic setup for Stock class

--- src/main/model/stock/Stock.java
+++ src/main/model/stock/Stock.java
@@ -1,5 +1,34 @@
 package main.model.stock;
 
 public class Stock{
+    private String name;
+    private double value;
 
+    public Stock(String name, double value){
+        this.name = name;
+        this.value = value;
+    }
+
+    public Stock (Stock source){
+        this.name = source.name;
+        this.value = source.value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    
 }
