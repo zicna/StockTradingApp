4e740b8160b6225142a6ff8f31e6032f32d7d4f0
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-05-05 11:18:28 -0500

    cleanup Main.java finish welcome, accCreation

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,36 +13,26 @@ import main.model.stock.Stock;
 import main.utils.Color;
 
 public class Main{
+
+    static Scanner scanner = new Scanner(System.in);
+    static Account account;
     public static void main(String[] args) {
         welcome();
-        Scanner scan = new Scanner(System.in);
-        while(true){
-            System.out.print("Respectively, type 'a' or 'b' to create a Personal account of TFSA: ");
-            String accountCreate = scan.nextLine();
-
-            if(accountCreate.equals("a")){
-                Personal personal = new Personal();
-                accCreateAnnouncement(personal);
+       
+        switch(accountChoice()){
+            case "a":
+                account = new Personal();
                 break;
-            }else if(accountCreate.equals("b")){
-                TFSA TFSA = new TFSA();
-                accCreateAnnouncement(TFSA);
+            case "b":
+                account = new TFSA();
                 break;
-            }else {
-            continue;
-            }
         }
+        accCreateAnnouncement(account);
         
-        System.out.print("Enter anything to start trading:");
-        scan.nextLine();
+        
 
-        dailyStockPrices();
 
-        for (int i = 0; i < 2160; i++) {
-            System.out.println("DAY " + i);
-            dailyStockPrices();
-        }
-        scan.close();
+        scanner.close();
     }
 
     public static void welcome(){
@@ -52,29 +42,21 @@ public class Main{
         System.out.println(Color.BLUE + "- Neither acount has a limit on the amount of trades that can be made.\n" + Color.RESET);
     }
 
+    public static String accountChoice(){
+        System.out.print("Respectively, type 'a' or 'b' to create a Personal account of TFSA: ");
+        String choice = scanner.nextLine();
+        while(!(choice.equals("a") || choice.equals("b"))){
+            System.out.print("Respectively, type 'a' or 'b' to create a Personal account of TFSA: ");
+            choice = scanner.nextLine();
+        }
+        return choice;
+    }
+
+
     public static <T extends Account> void accCreateAnnouncement(T source){
         if(!(source instanceof Account)){
             throw new IllegalArgumentException("arguments must be instance of Account class");
         }
             System.out.println("\nYou created a " + Color.YELLOW + source.getClass().getSimpleName() + Color.RESET + " account. Your account balance is " + Color.GREEN + source.getFunds() + Color.RESET + "\n");
-    }
-
-    public static void dailyStockPrices(){
-
-    //     Stock stock = new Stock();
-    //     Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource("src/main/data/" + "stock" + ".csv").toURI());
-    // * try make this work with AAPL stock only
-    // Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource("src/main/data/AAPL.csv").toURI());
-    Path path = Paths.get("src/main/data/AAPL.csv");
-    try {
-        Stream<String> stock = Files.lines(path);
-        stock
-            .map(line -> line.split(","))
-            .filter(lineArray -> lineArray[0] == "2")
-            .forEach(line -> System.out.print(line));
-        
-    } catch (Exception e) {
-        //TODO: handle exception
-    }
     }
 }
