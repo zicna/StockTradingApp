86b0a37a367fc6faa1f2413cf9a8d96d4ba14c74
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-05-12 11:26:51 -0500

    redo setFunds based of buyOrSell method outcome

--- src/main/model/account/Account.java
+++ src/main/model/account/Account.java
@@ -1,16 +1,17 @@
 package main.model.account;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.HashMap;
 
 import main.model.stock.Stock;
 
 public abstract class Account{
     private double funds;
-    private HashSet<Stock> portfolio;
+    private HashMap<String, Integer> portfolio;
 
     public Account(){
-        portfolio = new HashSet<>();
+        portfolio = new HashMap<>();
         this.funds = 4000;
     }
 
@@ -18,15 +19,24 @@ public abstract class Account{
         return funds;
     }
 
-    public HashSet<Stock> getPortfolio() {
+    public HashMap<String, Integer> getPortfolio() {
         return portfolio;
     }
 
-    public void setFunds(double funds) {
-        this.funds = funds;
+    public void setFunds(String chooice, double funds) {
+        if(chooice.equals("buy")){
+            this.funds -= funds;
+        }else{
+            this.funds += funds;
+        }
     }
-    public void setPortfolio(HashSet<Stock> portfolio) {
+    
+    public void setPortfolio(HashMap<String, Integer> portfolio) {
         this.portfolio = portfolio;
+    }
+
+    public void addStock(Stock stock, int numOfStocks){
+        portfolio.merge(stock.getName(), numOfStocks, Integer::sum);
     }
 
     public abstract void sellTrades();
