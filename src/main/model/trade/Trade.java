c7419d0331ed0ddf48bec0774adf3993b9917704
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-05-17 10:35:05 -0500

    add enums for stock name and choice (buy or sell)

--- src/main/model/trade/Trade.java
+++ src/main/model/trade/Trade.java
@@ -1,28 +1,34 @@
 package main.model.trade;
 
 public class Trade {
-    private String stockName;
+    private Stock stock;
     private double stockPrice;
     private int stockCount;
+    private Choice choice;
+
+    public enum Stock {FB, TSLA, AAPL, GOOG};
+    public enum Choice {BUY, SELL}
     
-    public Trade(String stockName, double stockPrice, int stockCount){
-        this.stockName = stockName;
+    public Trade(Stock stock, double stockPrice, int stockCount, Choice choice){
+        this.stock = stock;
         this.stockPrice = stockPrice;
         this.stockCount = stockCount;
+        this.choice = choice;
     }
-
+    
     public Trade(Trade source){
-        this.stockName = source.stockName;
+        this.stock = source.stock;
         this.stockPrice = source.stockPrice;
         this.stockCount = source.stockCount;
+        this.choice = source.choice;
     }
 
     public int getStockCount() {
         return stockCount;
     }
 
-    public String getStockName() {
-        return stockName;
+    public Stock getStock() {
+        return stock;
     }
 
     public double getStockPrice() {
@@ -33,8 +39,8 @@ public class Trade {
         this.stockCount = stockCount;
     }
 
-    public void setStockName(String stockName) {
-        this.stockName = stockName;
+    public void setStockName(Stock stock) {
+        this.stock = stock;
     }
 
     public void setStockPrice(double stockPrice) {
